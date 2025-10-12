4b2ee5a4dd7bc352b501864d4f9830ac8c7c181a
StrangeQuark <zacharyzirkle@hotmail.com>
2025-10-12 12:00:08 -0500

    Begin building ReactVault test suite

--- src/test/java/com/strangequark/reactservice/ReactVaultTests.java
+++ src/test/java/com/strangequark/reactservice/ReactVaultTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ReactVaultTests extends ReactTestsBase{
+public class ReactVaultTests extends ReactTestsBase {
     // Integration function start: Auth
     @Test
     public void ensureToolbarVaultButtonRedirectsToLoginTest() {
@@ -23,4 +23,53 @@ public class ReactVaultTests extends ReactTestsBase{
                 "toolbar without logging in");
     }
     // Integration function end: Auth
+    @Test
+    public void userCreateServiceTest() {
+        reactFunctions.registerEnableAndLogin(page, username, email, password); // Integration line: Auth
+        reactFunctions.navigateToVault(page);
+
+        reactFunctions.fillAndSubmitCreateServiceForm(page, serviceName);
+
+        page.getByText(serviceName).waitFor(WAIT_FOR_ATTACHED);
+
+        Locator serviceSelectOptions = page.locator("#service-select option");
+        assertTrue(serviceSelectOptions.allInnerTexts().contains(serviceName),
+                "Service name should be present in the dropdown after creation");
+    }
+
+    @Test
+    public void userCreateEnvironmentTest() {
+        reactFunctions.registerEnableAndLogin(page, username, email, password); // Integration line: Auth
+        reactFunctions.navigateToVault(page);
+
+        reactFunctions.fillAndSubmitCreateServiceForm(page, serviceName);
+        reactFunctions.selectService(page, serviceName);
+
+        reactFunctions.fillAndSubmitCreateEnvironmentForm(page, environmentName);
+
+        page.getByText(environmentName).waitFor(WAIT_FOR_ATTACHED);
+
+        Locator environmentSelectOptions = page.locator("#environment-select option");
+        assertTrue(environmentSelectOptions.allInnerTexts().contains(environmentName),
+                "Environment name should be present in the dropdown after creation");
+    }
+
+    @Test
+    public void userAddVariableTest() {
+        reactFunctions.registerEnableAndLogin(page, username, email, password); // Integration line: Auth
+        reactFunctions.navigateToVault(page);
+
+        reactFunctions.fillAndSubmitCreateServiceForm(page, serviceName);
+        reactFunctions.selectService(page, serviceName);
+
+        reactFunctions.fillAndSubmitCreateEnvironmentForm(page, environmentName);
+        reactFunctions.selectEnvironment(page, environmentName);
+
+        reactFunctions.fillAndSubmitAddVariableForm(page, testVariableKey, testVariableValue);
+
+        Locator variableKeyInput = page.locator("input[value='" + testVariableKey + "']");
+
+        variableKeyInput.waitFor(WAIT_FOR_VISIBLE);
+        assertTrue(variableKeyInput.isVisible(), "Variable key input should be visible after variable creation");
+    }
 }
