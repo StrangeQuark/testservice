f30c8560906d45b68ee8f1a9ddeca5b05594119d
StrangeQuark <zacharyzirkle@hotmail.com>
2026-09-05 19:23:28 -0500

    Add React security headers

--- src/test/java/com/strangequark/reactservice/ReactTests.java
+++ src/test/java/com/strangequark/reactservice/ReactTests.java
@@ -5,6 +5,7 @@ package com.strangequark.reactservice;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReactTests extends ReactTestsBase {
@@ -16,5 +17,18 @@ public class ReactTests extends ReactTestsBase {
 
         homeButton.waitFor(WAIT_FOR_VISIBLE);
         assertTrue(homeButton.isVisible(), "Home button should be visible in the toolbar after navigating to home page");
+    }
+
+    @Test
+    public void securityHeadersTest() {
+        APIResponse response = apiRequestContext.get(ReactFunctions.REACT_BASE_URL);
+
+        assertEquals(200, response.status());
+        assertTrue(response.headers().get("content-security-policy").contains("default-src 'self'"));
+        assertEquals("same-origin", response.headers().get("cross-origin-opener-policy"));
+        assertEquals("nosniff", response.headers().get("x-content-type-options"));
+        assertEquals("DENY", response.headers().get("x-frame-options"));
+        assertEquals("strict-origin-when-cross-origin", response.headers().get("referrer-policy"));
+        assertEquals("camera=(), geolocation=(), microphone=(), payment=(), usb=()", response.headers().get("permissions-policy"));
     }
 }
